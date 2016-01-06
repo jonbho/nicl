cdd904695b1188792b183f29645138274ffe5688
jonbho <jon@symnum.com>
2016-01-06 18:36:11 +0100

    Initial working version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# nicl
-Node.js Interactive Command Line
+nicl(1) -- Node.js Interactive Command Line
+===========================================
+
+INSTALLING
+----------
+
+### via npm
+* `npm install nicl`
+* Should be that simple.
+
+### from source
+* `git clone git://github.com/jonbho/nicl.git`
+* `cd nicl`
+* `npm install`
+
+You may need to install `fibers` separately (npm fibers).
+
+RATIONALE
+----------
+`Nicl` helps you write a JavaScript interactive command-line application in the "classic" style using Node.js.
+By "classic" style, I mean a linear sequence of code that can ask the user for input and print results in
+the order it wants. This is easy by default in most programming languages, but it is hard in JavaScript,
+since its native environemnt, eg, the browser, is not conducive to command-line aplications. And the
+de-facto standard for non-browser JavaScript is Node.js, which is designed as an event-driven framework
+that doesn't allow you sequential code interacting with the users.
+
+Many IDEs only understad "browser JavaScript" and "Node.js JavaScript". If you want to benefit from
+their advantages foro your JavaScript project (autocompletion, debugging, etc...), your project had
+better be one of those two. 
+
+The main initial goal is educational. Using `nicl` and a free IDE like NetBeans, a beginner can start writing
+simple command-line applications in JavaScript, using code completion and the debugger, and complete many
+exercises until they understand variables, conditions, loops, function-calls, etc...
+
+EXAMPLE
+-------
+Here is a sample application using `nicl` to ask the user a couple questions and respond politely, in sequence:
+
+```javascript
+var nicl = require("nicl");
+
+function main() {
+    nicl.printLine("Hello, what is your name?");
+    var name = nicl.readLine();
+    nicl.printLine("Great to meet you, " + name + "!");
+    
+    nicl.printLine("Where are you?");
+    var place = nicl.readLine();
+    nicl.printLine("Good. Hope it's all fine in " + place + "!");
+    
+    process.exit(0);
+}
+        
+nicl.run(main);
+```
+
+Note how you need to run the code that uses `nicl` by calling `nicl.run()` and passing it the function to execute.
+Exiting the program needs to be done explicitly if so desired, as Node.js won't by default close the application
+until explicitly told to do so.
+
+SUPPORTED ENVIRONMENTS
+----------------------
+`nicl` has been tested to work in OS X 10.10 Yosemite, both inside the NetBeans IDE console, and from the command line,
+by using `node main.js` to launch a program. Beware, it won't work properly inside the Node REPL, since
+the REPL sets input in "raw" mode (input processed at the keystroke level), and `nicl` is designed for cooked mode
+(linewise input).
+
+TECHNICAL DETAILS
+-----------------
+`nicl` builds on [node-fibers](https://github.com/laverdet/node-fibers)
+by [Marcel Laverdet](https://github.com/laverdet), a coroutine-style library that allows adding suspension
+and resumption sematincs inside a single-threaded, event-driven library like Node.js, where blocking I/O
+is in principle not possible.
+
+
+FUTURE EVOLUTION
+----------------
+Support for raw input (keypress-level) will probably be added in the future.
+
+LEGAL
+-----
+(c) January 2016 Jon Beltran de Heredia (jonbho). Licensed under the GPL v2.0 license.
---
+++ nicl.js
@@ -0,0 +1,110 @@
+/*
+    nicl - Node.js Interactive Command Line
+
+    Linear, interactive command-line support for Node.js. Allows you to write
+    software using a classic-style main() function, calling nicl.printline()
+    and nicl.readLine() to interact with the user. Turns Javascript + Node.js
+    into a good, simple learning environment for beginners.
+
+    It uses Fibers (https://github.com/laverdet/node-fibers) by Marcel Laverdet
+    to simulate blocking, linear execution in an event-based environment like
+    Node.js.
+
+    Copyright (C) 2016 Jon Beltran de Heredia
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+// Other modules used
+var Fiber = require("fibers");
+
+// Buffer to store input while not requested, and fiber to call when there's something
+var initialized = false;
+var inputBuffer = "";
+var inputListenerFiber = null;
+
+/* This function sets up input processing such that whatever is received via stdin
+ * is stored in the inputBuffer, and if the main "nicl'd" code fiber is suspended
+ * waitin for input, it will be woken up to resume its process */
+function ensure_initial_setup() {
+    if (!initialized) {
+        initialized = true;
+        process.stdin.resume();
+        process.stdin.setEncoding("utf8");
+        process.stdin.on("data", function (text) {
+            // Store input in buffer
+            inputBuffer += text;
+
+            // After input received, if main fiber was waiting, wake it up
+            if (inputListenerFiber)
+                inputListenerFiber.run();
+        });
+    }
+}
+
+/* This function is called from the "main" nicl'd code to ask for a line of input.
+ * If there is input available in the buffer from previously read data, it will
+ * remove it from the buffer and return it to the caller directly. If there is
+ * no input available, it will "suspend" this fiber of execution, to be woken
+ * back up when input arrives, and continue running the main "nicl'd" code. */
+function readLine() {
+    // Ensure asynchronous operation is set up
+    ensure_initial_setup();
+
+    // Set up listener for when input arrives (it just continues on the next step)
+    var fiber = Fiber.current;
+    inputListenerFiber = Fiber(function () {
+        fiber.run();
+    });
+
+    // If there isn't any, terminate this fiber until there is
+    if (inputBuffer === "")
+    {
+        // Register a listener to continue the work
+        Fiber.yield();
+    }
+
+    // There was input, remove listener
+    inputListenerFiber = null;
+
+    // Take first line of input and return it to caller
+    var ix = inputBuffer.indexOf("\n");
+    var ixAfterLine = ix + 1;
+    if (ix === -1) {
+        ix = inputBuffer.length;
+        ixAfterLine = ix;
+    }
+    var firstLine = inputBuffer.slice(0, ix);
+    inputBuffer = inputBuffer.slice(ixAfterLine);
+
+    return firstLine;
+}
+
+/* Simple wrapper so that all command-line interaction can be done via nicl */
+function printLine(str) {
+    console.log(str);
+}
+
+/* This is the main entry point, pass-in the function that uses readLine()
+ * so that it is executed "linearly", as if readLine() was blocking I/O, by
+ * the magic of suspending/resuming fibers depending on the availability of input. */
+function run(fn) {
+    Fiber(fn).run();
+}
+
+/* Export all the user-available functions */
+exports.readLine  = readLine;
+exports.printLine = printLine;
+exports.run       = run;
